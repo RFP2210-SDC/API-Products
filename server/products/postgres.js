11addca4a19f12d6c97ba29c8836ef4bfbca54e0
Ryan Gehris <rogehris@gmail.com>
2022-12-22 14:59:13 -0800

    working on postgres

---
+++ server/products/postgres.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  password: "one"
+})
+
+
+const doWork = async function() {
+  await client.connect()
+  try {
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+doWork();
