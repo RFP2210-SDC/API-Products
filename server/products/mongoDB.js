7ac15ab49d8ed158930dce013fa95b9b0d015c7e
Ryan Gehris <rogehris@gmail.com>
2022-12-21 22:41:52 -0800

    schema is working. Could not get timestamps or default to work

--- server/products/mongoDB.js
+++ server/products/mongoDB.js
@@ -5,37 +5,43 @@ mongoose.set('strictQuery', true);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  const styleSchema = new mongoose.Schema({
+  const StyleSchema = new mongoose.Schema({
     style_id: Number,
     name: String,
     original_price: Number,
     sale_price: Number,
     default_style: Boolean,
     photos: [{thumbnail_url: String, url: String}],
-    skus: [{id: {size: String, quantity: Number}}]
+    skus: [Object]
   });
 
-  const productSchema = new mongoose.Schema({
+  const ProductSchema = new mongoose.Schema({
     product_id: Number,
     name: String,
-    campus: {type: String, default: "hr-rfp"},
+    // campus: {type: String, default: "hr-rfp"},
     slogan: String,
     description: String,
     category: String,
     default_price: Number,
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now},
     features: [{feature: String, value: String}],
-    // results: [styleSchema],
     related: [Number],
-  });
+    results: [StyleSchema]
+  }, {timestamps: true});
 
-  const Product = mongoose.model('Product', productSchema);
+  const Product = mongoose.model('Product', ProductSchema);
 
-  const Style = mongoose.model('Style', styleSchema);
+  // let abc = new Product({product_id: 1, name: "pants", slogan: "Hello World", description: "They are so fancy", category: "mens", default_price: 10, features: [{feature: "yellow", value: "cotton"}, {feature: "black", value: "leather"}], related: [1,2,3,4], campus: undefined, results: {style_id: 123445, name: "coco puffs", original_price: 10, sale_price: 1, default_style: false, photos: [{thumbnail_url: "xxxxx", url: "yyyyyy"}], skus: [{9999: {quantity: 10, size: "xs"}}]}});
+
+  // setTimeout(function() {
+  //   console.log('tell me ', abc.createdAt);
+  // }, 3000);
+
+  // const Style = mongoose.model('Style', StyleSchema);
 })
 
 // to create database use-> node /Users/RyanGehris/hack-reactor-sdc/API-Products/server/products/mongoDB.js
 // to show databases use-> show databases
 // to use DB-> use <database>
 // to show collections-> show collections
+
+// db.products.insertOne({product_id: 1, name: "pants", slogan: "Hello World", description: "They are so fancy", category: "mens", default_price: 10, features: [{feature: "yellow", value: "cotton"}, {feature: "black", value: "leather"}], related: [1,2,3,4], campus: undefined, results: {style_id: 123445, name: "coco puffs", original_price: 10, sale_price: 1, default_style: false, photos: [{thumbnail_url: "xxxxx", url: "yyyyyy"}], skus: [{9999: {quantity: 10, size: "xs"}}]}});
