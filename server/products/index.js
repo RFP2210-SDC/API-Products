088dd944056b9b178f6d43d5b25e1b8f2c825a68
Ryan Gehris <rogehris@gmail.com>
2023-01-03 11:01:17 -0800

    confirmed server requests are working. Created file for DB queries.

--- server/products/index.js
+++ server/products/index.js
@@ -8,19 +8,34 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/products', (req, res) => {
-  //SELECT * FROM product
+  // SELECT * FROM product
+  //   returns id, name, slogan, description, category, default_price
+  //   include count and page
+  res.status(200).send('product list');
 })
 
 app.get('/products/:product_id', (req, res) => {
-  //SELECT * FROM product WHERE product_id=${product_id}
+  // SELECT * FROM product WHERE product_id=${product_id}
+  //   returns id, name, slogan, description, category, default_price, features (array)
+  res.status(200).send('individual product');
 })
 
 app.get('/products/:product_id/styles', (req, res) => {
-  //SELECT * FROM style WHERE style_product_id=${product_id}
+  // SELECT * FROM style WHERE style_product_id=${product_id}
+  //   returns product_id, results (obj)
+  //     within results object: style_id, name, original_price, sale_price, default, photos (array of objects), skus (obj)
+  res.status(200).send('styles');
 })
 
 app.get('/products/:product_id/related', (req, res) => {
-  //SELECT related_product_id FROM related WHERE current_product_id=${product_id};
+  // SELECT related_product_id FROM related WHERE current_product_id=${product_id};
+  //   returns an array of product_ids
+  if (req.params.product_id === '1') {
+    res.status(200).send('related 1');
+  }
+  if (req.params.product_id === '5') {
+    res.status(200).send('related 5');
+  }
 })
 
 app.listen(port, () => {
