6e97a5281c3e52e4dcee12c8101faae3ee522cc8
Ryan Gehris <rogehris@gmail.com>
2023-01-03 22:33:40 -0800

    created query to return styles object, minus skus

--- server/products/postgres.js
+++ server/products/postgres.js
@@ -77,7 +77,8 @@ module.exports.getStyles = function(product_id, client, done, cb) {
   //   returns product_id, results (obj)
   //     within results object: style_id, name, original_price, sale_price, default, photos (array of objects), skus (obj)
   const queryStyles = {
-    text: 'SELECT style_id, name, original_price, sale_price, default_style FROM style WHERE style_product_id = $1',
+    text: 'SELECT style_id, name, original_price, sale_price, default_style, JSONB_AGG(JSONB_BUILD_OBJECT(\'url\', url, \'thumbnail_url\', thumbnail_url)) AS photos FROM style LEFT JOIN photo ON style_id = photo_style_id WHERE style_product_id = $1 GROUP BY style_id',
+    // text: 'SELECT style_id, name, original_price, sale_price, default_style, JSONB_AGG(JSONB_BUILD_OBJECT(\'url\', url, \'thumbnail_url\', thumbnail_url)) AS photos, sku.id, quantity, size FROM style LEFT JOIN photo ON style_id = photo_style_id LEFT JOIN sku ON style_id = sku_style_id WHERE style_product_id = $1 GROUP BY style_id',
     values: [product_id],
   }
 
@@ -87,9 +88,21 @@ module.exports.getStyles = function(product_id, client, done, cb) {
   }
 
   const querySku = {
-    text: 'SELECT id, sku_style_id, size, quantity FROM sku INNER JOIN style ON sku_style_id = style_id AND style_product_id = $1'
+    text: 'SELECT id, sku_style_id, size, quantity FROM sku INNER JOIN style ON sku_style_id = style_id AND style_product_id = $1',
     values: [product_id],
   }
+
+  const result = {product_id: product_id}
+  client.query(queryStyles)
+    .then(res => {
+      Object.assign(result, {results: res.rows});
+      cb(null, result);
+    })
+    .catch(err => {
+      console.error(err);
+      cb(err);
+    })
+    .finally(() => done())
 }
 
 module.exports.getRelated = function(product_id, client, done, cb) {
