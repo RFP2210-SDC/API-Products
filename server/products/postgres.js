21f5034ada08e315810418ac0ee46b64dae5243d
Ryan Gehris <rogehris@gmail.com>
2023-01-04 00:01:48 -0800

    updated query for getProduct

--- server/products/postgres.js
+++ server/products/postgres.js
@@ -42,28 +42,15 @@ module.exports.getProductList = function(params, client, done, cb) {
 }
 
 module.exports.getProduct = function(product_id, client, done, cb) {
-  // SELECT * FROM product WHERE product_id=${product_id}
-    //   returns id, name, slogan, description, category, default_price, features (array)
-  const productQuery = {
-    text: 'SELECT * FROM product WHERE product_id = $1',
+  const query = {
+    text: 'SELECT product_id, name, slogan, description, category,default_price, JSONB_AGG(JSONB_BUILD_OBJECT(\'feature\', feature, \'value\', value)) AS features FROM product LEFT JOIN feature ON product_id = feature_product_id WHERE product_id = $1 GROUP BY product_id',
     values: [product_id]
   }
 
-  const featureQuery = {
-    text: 'SELECT feature, value FROM feature WHERE feature_product_id = $1',
-    values: [product_id]
-  }
-
-  const result = {}
   client
-    .query(productQuery)
+    .query(query)
     .then(res => {
-      Object.assign(result, res.rows[0])
-      return client.query(featureQuery)
-    })
-    .then(res => {
-      Object.assign(result, {feature: [res.rows]});
-      cb(null, result);
+      cb(null, res.rows[0]);
     })
     .catch(err => {
       console.error(err);
