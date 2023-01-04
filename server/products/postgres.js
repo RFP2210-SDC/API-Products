7089813c008132352e3ec936cf75aea293ae0f7e
Ryan Gehris <rogehris@gmail.com>
2023-01-03 23:52:23 -0800

    completed style query

--- server/products/postgres.js
+++ server/products/postgres.js
@@ -73,29 +73,25 @@ module.exports.getProduct = function(product_id, client, done, cb) {
 }
 
 module.exports.getStyles = function(product_id, client, done, cb) {
-  // SELECT * FROM style WHERE style_product_id=${product_id}
-  //   returns product_id, results (obj)
-  //     within results object: style_id, name, original_price, sale_price, default, photos (array of objects), skus (obj)
   const queryStyles = {
     text: 'SELECT style_id, name, original_price, sale_price, default_style, JSONB_AGG(JSONB_BUILD_OBJECT(\'url\', url, \'thumbnail_url\', thumbnail_url)) AS photos FROM style LEFT JOIN photo ON style_id = photo_style_id WHERE style_product_id = $1 GROUP BY style_id',
-    // text: 'SELECT style_id, name, original_price, sale_price, default_style, JSONB_AGG(JSONB_BUILD_OBJECT(\'url\', url, \'thumbnail_url\', thumbnail_url)) AS photos, sku.id, quantity, size FROM style LEFT JOIN photo ON style_id = photo_style_id LEFT JOIN sku ON style_id = sku_style_id WHERE style_product_id = $1 GROUP BY style_id',
     values: [product_id],
   }
-
-  const queryPhotos = {
-    text: 'SELECT thumbnail_url, url, style_id FROM photo INNER JOIN style ON photo_style_id = style_id AND style_product_id = $1',
-    values: [product_id],
-  }
-
+  // cannot next JSON_AGG inside of jsonb_object_agg(name, value)
   const querySku = {
-    text: 'SELECT id, sku_style_id, size, quantity FROM sku INNER JOIN style ON sku_style_id = style_id AND style_product_id = $1',
-    values: [product_id],
+    text: 'SELECT style_id, sku.id, quantity, size FROM style  LEFT JOIN sku ON style_id = sku_style_id WHERE style_product_id = $1',
+    values: [product_id]
   }
 
   const result = {product_id: product_id}
   client.query(queryStyles)
     .then(res => {
       Object.assign(result, {results: res.rows});
+      return client.query(querySku)
+    })
+    .then(res => {
+      newResult = helperFunction(result.results, res.rows)
+      result.results = newResult
       cb(null, result);
     })
     .catch(err => {
@@ -126,4 +122,24 @@ module.exports.getRelated = function(product_id, client, done, cb) {
       cb(err);
     })
     .finally(() => done())
+}
+
+
+const helperFunction = function(arr, arr2) {
+  const skuObj = {};
+  arr2.forEach((row) => {
+    if (!skuObj[row.style_id]) {
+      var newObj = {}
+      newObj[row.id] = {quantity: row.quantity, size: row.size}
+      skuObj[row.style_id] = newObj;
+    } else {
+      var newObj = {}
+      newObj[row.id] = {quantity: row.quantity, size: row.size}
+      Object.assign(skuObj[row.style_id], newObj)
+    }
+  })
+  arr.forEach((style) => {
+    style.skus = skuObj[style.style_id];
+  })
+  return arr;
 }
